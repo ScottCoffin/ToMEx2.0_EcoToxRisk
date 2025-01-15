08b93efeb25961b47046ee3182e1e8df6dd546b7
magdalenammair <43850321+magdalenammair@users.noreply.github.com>
2025-01-15 15:30:31 +0100

    figure update 1/2

--- ToMEx2.0_MPcharacteristics/Code/02_ToMEx_descriptive_plots.R
+++ ToMEx2.0_MPcharacteristics/Code/02_ToMEx_descriptive_plots.R
@@ -17,15 +17,6 @@ fresh = dat[dat$environment == "Freshwater",]
 marine = dat[dat$environment == "Marine",]
 
 
-# We need summary stats and plot:
-
-# - charge
-
-
-
-
-
-
 # Polymer-----
 
 (polyfresh = fresh %>%  group_by(poly_group) %>% summarize(count = n()) %>% 
@@ -70,26 +61,131 @@ op = par(bty = "l", mfrow = c(1,2))
 boxplot(dat$size.length.um.used.for.conversions ~ dat$environment,
         bty = "l", las = 2, xlab = "", pch = "°",
         col = c("lightskyblue", "royalblue3")) #,border = "grey60"
+summary(fresh$size.length.um.used.for.conversions)
+sd(fresh$size.length.um.used.for.conversions, na.rm = TRUE)
+summary(marine$size.length.um.used.for.conversions)
+sd(marine$size.length.um.used.for.conversions, na.rm = TRUE)
 
 # Particle width -----
 boxplot(dat$size.width.um.used.for.conversions ~ dat$environment,
         bty = "l", las = 2, xlab = "", pch = "°",
         col = c("lightskyblue", "royalblue3"))
+summary(fresh$size.width.um.used.for.conversions)
+sd(fresh$size.width.um.used.for.conversions, na.rm = TRUE)
+summary(marine$size.width.um.used.for.conversions)
+sd(marine$size.width.um.used.for.conversions, na.rm = TRUE)
 
 par(op)
 
 
 # Sodium.azide ----
 
+(sodfresh = fresh %>%  group_by(sodium.azide) %>% summarize(count = n()) %>% 
+   mutate(freq = count/sum(count))) 
+(sodmarine = marine %>%  group_by(sodium.azide) %>% summarize(count = n()) %>% 
+    mutate(freq = count/sum(count)))
+sodmerged = merge(sodfresh, sodmarine, by = "sodium.azide", sort = FALSE)
+(sodmerged = sodmerged %>% arrange(desc(freq.x), .by_group = TRUE))
 
+sodbar = cbind(sodmerged$freq.x, sodmerged$freq.y)
+sodbar= as.matrix(t(sodbar))
+colnames(sodbar) = sodmerged$sodium.azide
+
+op = par(mar = c(12,3,1,1), mfrow = c(1,4))
+barplot(sodbar, beside = TRUE, las = 2, col = c("lightskyblue", "royalblue3"),
+        ylim = c(0,1))
+legend("topright", legend = c("Freshwater", "Marine"), 
+       fill = c("lightskyblue", "royalblue3"), bty = "n")
+par(op)
 
 # DOM_present ----
 
+(domfresh = fresh %>%  group_by(DOM_present) %>% summarize(count = n()) %>% 
+   mutate(freq = count/sum(count))) 
+(dommarine = marine %>%  group_by(DOM_present) %>% summarize(count = n()) %>% 
+    mutate(freq = count/sum(count)))
+dommerged = merge(domfresh, dommarine, by = "DOM_present", sort = FALSE)
+(dommerged = dommerged %>% arrange(desc(freq.x), .by_group = TRUE))
+
+dombar = cbind(dommerged$freq.x, dommerged$freq.y)
+dombar= as.matrix(t(dombar))
+colnames(dombar) = dommerged$DOM_present
+
+#op = par(mar = c(12,3,1,1))
+barplot(dombar, beside = TRUE, las = 2, col = c("lightskyblue", "royalblue3"),
+        ylim = c(0,1))
+legend("topright", legend = c("Freshwater", "Marine"), 
+       fill = c("lightskyblue", "royalblue3"), bty = "n")
+#par(op)
 
 # charge ----
+(chargefresh = fresh %>%  group_by(charge) %>% summarize(count = n()) %>% 
+   mutate(freq = count/sum(count))) 
+(chargemarine = marine %>%  group_by(charge) %>% summarize(count = n()) %>% 
+    mutate(freq = count/sum(count)))
+chargemerged = merge(chargefresh, chargemarine, by = "charge", sort = FALSE)
+(chargemerged = chargemerged %>% arrange(desc(freq.x), .by_group = TRUE))
 
+chargebar = cbind(chargemerged$freq.x, chargemerged$freq.y)
+chargebar= as.matrix(t(chargebar))
+colnames(chargebar) = chargemerged$charge
+
+#op = par(mar = c(12,3,1,1))
+barplot(chargebar, beside = TRUE, las = 2, col = c("lightskyblue", "royalblue3"),
+        ylim = c(0,1))
+legend("topright", legend = c("Freshwater", "Marine"), 
+       fill = c("lightskyblue", "royalblue3"), bty = "n")
+#par(op)
 
 # funcional group ----
 
+(functfresh = fresh %>%  group_by(functional.group) %>% summarize(count = n()) %>% 
+   mutate(freq = count/sum(count))) 
+(functmarine = marine %>%  group_by(functional.group) %>% summarize(count = n()) %>% 
+    mutate(freq = count/sum(count)))
+functmerged = merge(functfresh, functmarine, by = "functional.group", sort = FALSE)
+(functmerged = functmerged %>% arrange(desc(freq.x), .by_group = TRUE))
+
+functbar = cbind(functmerged$freq.x, functmerged$freq.y)
+functbar= as.matrix(t(functbar))
+colnames(functbar) = functmerged$funct
+
+#op = par(mar = c(12,3,1,1))
+barplot(functbar, beside = TRUE, las = 2, col = c("lightskyblue", "royalblue3"),
+        ylim = c(0,1))
+legend("topright", legend = c("Freshwater", "Marine"), 
+       fill = c("lightskyblue", "royalblue3"), bty = "n")
+par(op)
+
+## combined descriptive figure
+
+layout(matrix(1:8, ncol = 4, byrow = TRUE))
+
+op = par(mar = c(12,4,1,1), bty = "l")
+
+# A - polymer
+barplot(polybar, beside = TRUE, las = 2, col = c("lightskyblue", "royalblue3"),
+        ylim = c(0,1), ylab = "Proportion")
+legend("topright", legend = c("Freshwater", "Marine"), 
+       fill = c("lightskyblue", "royalblue3"), bty = "n")
+text("A", x = 2, y = 0.97, cex = 1.5, font = 1)
+
+# B - shape
+barplot(shapebar, beside = TRUE, las = 2, col = c("lightskyblue", "royalblue3"),
+        ylim = c(0,1), ylab = "Proportion")
+text("B", x = 2, y = 0.97, cex = 1.5, font = 1)
+
+# C - length
+boxplot(dat$size.length.um.used.for.conversions ~ dat$environment,
+        bty = "l", las = 2, xlab = "", pch = "°",
+        col = c("lightskyblue", "royalblue3"),
+        ylab = "Particle length in µm") 
+text("C", x = 0.6, y = 400, cex = 1.5, font = 1)
 
 
+# D - width
+boxplot(dat$size.width.um.used.for.conversions ~ dat$environment,
+        bty = "l", las = 2, xlab = "", pch = "°",
+        col = c("lightskyblue", "royalblue3"),
+        ylab = "Particle width in µm")
+text("D", x = 0.6, y = 350, cex = 1.5, font = 1)
--- ToMEx2.0_MPcharacteristics/Code/04_NMDS_MP_comparison.R
+++ ToMEx2.0_MPcharacteristics/Code/04_NMDS_MP_comparison.R
@@ -4,9 +4,9 @@ library(vegan)
 # Import data ---
 
 dat = read.csv("Data/data_comp_to_env_compiled.csv", stringsAsFactors = TRUE)
-str(dat)
-summary(dat)
-levels(dat$doi)
+# str(dat)
+# summary(dat)
+# levels(dat$doi)
 
 #dat = dat[!grepl("Kooi", dat$source), ]
 # dat$tomex_binary = NA
@@ -15,13 +15,14 @@ levels(dat$doi)
 
 # scale variables
 colnames(dat)
-vars = dat[,c(5:17,19)]
+vars = dat[,c(5:16,18)]
 scaled = scale(vars)
 
 # replace all NAs with 0 - set all unknown values to variable mean
 scaled[is.na(scaled)] = 0
 
 # NMDS
+set.seed(2024)
 NMDS = metaMDS(scaled, distance="euclidean", k=2, autotransform=F, wascores=F)
 NMDS$points
 x <- NMDS$points[,1]      
@@ -30,19 +31,22 @@ y <- NMDS$points[,2]
 # get vectors for each variable
 envfitall <- envfit(NMDS, scaled) 
 vectors = as_tibble(envfitall$vectors$arrows, rownames = "properties")
-vectors$vectornames = c("PE/PET/Polyester", "PP", "PS", "PA", "PU", "Acrylates", "PVC/PVA", "PTFE", "Other",
-                        "Fragments", "Spheres", "Fibers", "Mean length", "Mean width")
-
-adjust.x = c(-0.2, 0.2, -0.2, -0.35, 0.4, 0.4, -0.1, -0.4, 0.4, -0.6, -0.5, -0.4, -0.8, 0.65)
-adjust.y = c(-0.15, 0.1, 0, -0.05, -0.1, 0.15, 0.3, 0, 0.1, -0.15, 0.1, 0, 0, -0.15)
+vectors$vectornames = c("PE/PET/Polyester", "PP", "PS", "PA", "PU", "PVC/PVA", "PTFE", "Other",
+                        "Fragments",    "Spheres", "Fibers", "Mean length", "Mean width")
 
 palette(c("lightskyblue", "royalblue3"))
-op = par(bty = "l", las = 1, cex = 1.2)
+
+adjust.x = c(-1.3, 0.3, -0.3, 0, 0.3, 0.7, 0, 0.2, -0.5,   -0.6, 0.15, -0.8, -0.8)
+adjust.y = c(-0.1, 0, 0, -0.2, -0.1, 0.1, 0.2, -0.15, 0.2,       0, -0.3, -0.1, 0)
+
+png("Plots/NMDS_environmental.png", width = 18, height = 15, units = "cm", res = 1000)
+
+op = par(bty = "l", las = 1, cex = 1)
 
 plot(x,y, bg = adjustcolor(as.numeric(dat$fresh_marine_binary), alpha.f = 0.9), col = c(rep("red",2), rep("grey50",(nrow(dat)-2))), 
      pch = c(rep(24,2), rep(21,(nrow(dat)-2))), lwd = c(2,2, rep(1, (nrow(dat)-2))),
      xlab = "NMDS1", ylab = "NMDS2", cex = 1.5, 
-     ylim = c(-4,3), xlim = c(-8, 4))
+     ylim = c(-4,4), xlim = c(-8, 4))
 
 for(i in 1:nrow(envfitall$vectors$arrows)){
 arrows(0,0, envfitall$vectors$arrows[i,1]*2.5, envfitall$vectors$arrows[i,2]*2.5, 
@@ -50,9 +54,10 @@ arrows(0,0, envfitall$vectors$arrows[i,1]*2.5, envfitall$vectors$arrows[i,2]*2.5
 text(envfitall$vectors$arrows[i,1]*2.5 + adjust.x[i], envfitall$vectors$arrows[i,2]*2.5 + adjust.y[i],
      vectors$vectornames[i], cex = 0.8, col = "grey50")
 }          
-text("ToMEx 2.0", x = c(-3.8, -2), y = c(-4.1, -1.6), col = "red",
+text("ToMEx 2.0", x = -6, y = -0.2, col = "red",
      cex = 0.8, pos = 4, font = 2)
 
+dev.off()
 
 #### END ####
 
